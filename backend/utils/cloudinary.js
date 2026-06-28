878bda77d08219ff0c2798047dcf10feb56c1ab7
vais2004 <vkawale2004@gmail.com>
2026-06-28 23:46:54 +0530

    update feat: in utils folder created an cloudinary page and wrote the code for upload to cloudinary and delete from cloudinary

---
+++ backend/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+//configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+//to upload files to cloudinary
+export async function uploadToCloudinary(filePath, folder = "Doctor") {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder,
+      resource_type: "image",
+    });
+
+    //remove the local file after upload
+    fs.unlinkSync(filePath);
+    return result;
+  } catch (error) {
+    console.error("Cloudinary upload error:", error);
+    throw error;
+  }
+}
+
+//to delete image that is present in cloudinary if user removes from the UI
+export async function deleteFromCloudinary(publicId) {
+  try {
+    if (!publicId) return;
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.error("cloudinary delete error:", error);
+    throw error;
+  }
+}
+
+export default cloudinary;
