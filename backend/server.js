02ccc12d0bb4a2d76b25cd6e80394c1bf3498c01
vais2004 <vkawale2004@gmail.com>
2026-06-28 18:07:35 +0530

    importing the dependencies and wrote code for PORT and Get for API WORKING

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,26 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import { clerkMiddleware } from "@clerk/express";
+import { connectDB } from "./config/db.connect.js";
+
 const app = express();
 const port = 4000;
+
+//middleware
+app.use(cors());
+app.use(clerkMiddleware());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+
+//db
+connectDB()
+
+//routes
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on the http://localhost:${port}`);
+});
