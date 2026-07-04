93360062acbf8954b21b11d20023ecdfc8be9abd
vais2004 <vkawale2004@gmail.com>
2026-07-04 14:32:58 +0530

    update feat: to update a service

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -144,3 +144,65 @@ export async function getServiceById(req, res) {
     return res.status(500).json({ success: false, message: "Server Error" });
   }
 }
+
+//to update a service
+export async function updateService(req, res) {
+  try {
+    const { id } = req.params;
+    const existing = await Service.findById(id);
+    if (!existing)
+      return res.status(404).json({
+        success: false,
+        message: "Service not found",
+      });
+
+    const b = req.body || {};
+    const updateData = {};
+
+    //to update each field if already present then update them
+    if (b.name !== undefined) updateData.name = b.name;
+    if (b.about !== undefined) updateData.about = b.about;
+    if (b.shortDescription !== undefined)
+      updateData.shortDescription = b.shortDescription;
+    if (b.price !== undefined) updateData.price = sanitizePrice(b.price);
+    if (b.availability !== undefined)
+      updateData.available = parseAvailability(b.availability);
+    if (b.instructions !== undefined)
+      updateData.instructions = parseJsonArrayField(b.instructions);
+    if (b.slots !== undefined)
+      updateData.slots = normalizeSlotsToMap(parseJsonArrayField(b.slots));
+
+    if (req.file) {
+      try {
+        const up = await uploadToCloudinary(req.file.path, "services");
+        if (up?.secure_url) {
+          updateData.imageUrl = up.secure_url;
+          updateData.imagePublicId = up.public_id || null;
+          if (existing.imagePublicId) {
+            //it will remove the old image and replace it with the new
+            try {
+              await deleteFromCloudinary(existing.imagePublicId);
+            } catch (err) {
+              console.warn("Cloudinary delete failed:", err?.message || err);
+            }
+          }
+        }
+      } catch (err) {
+        console.error("Cloudinary upload error:", err);
+      }
+    }
+
+    const updated = await Service.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({
+      success: true,
+      data: updated,
+      message: "Service Updated",
+    });
+  } catch (error) {
+    console.error("updateService Error:", error);
+    return res.status(500).json({ success: false, message: "Server Error" });
+  }
+}
