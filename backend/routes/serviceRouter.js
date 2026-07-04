d7d0980881cb4fe62ce638807f00f2e8e916b29a
vais2004 <vkawale2004@gmail.com>
2026-07-04 15:24:10 +0530

    update feat: wrote routes for the service as of put post get delete

---
+++ backend/routes/serviceRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+import multer from "multer";
+
+import {
+  createService,
+  deleteService,
+  getServiceById,
+  getServices,
+  updateService,
+} from "../controllers/serviceController";
+
+const upload = multer({ dest: "/tmp" });
+const serviceRouter = express.Router();
+
+serviceRouter.get("/", getServices);
+serviceRouter.get("/:id", getServiceById);
+
+serviceRouter.post("/", upload.single("image"), createService);
+serviceRouter.put("/:id", upload.single("image"), updateService);
+
+serviceRouter.delete("/:id", deleteService);
+
+export default serviceRouter;
