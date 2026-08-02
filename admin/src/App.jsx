78c5e2a5cbd6c18b4b2fdeb3cd52f2d716cea0a1
vais2004 <vkawale2004@gmail.com>
2026-08-02 22:51:11 +0530

    update feat: removed extra code and impoting Routes and Route from the react router dom

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import { Route, Routes } from "react-router-dom";
+import { Hero } from "./pages/Hero";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>skd</h1>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Hero />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
