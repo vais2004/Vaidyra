438104902c6b0afbbb48547538604e6168b820bb
vais2004 <vkawale2004@gmail.com>
2026-08-02 22:51:46 +0530

    update feat: created an page for Navbar

---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { navbarStyles as ns } from "../assets/dummyStyles";
+import logoImg from "../assets/logo.png";
+
+export const Navbar = () => {
+  return <div>Navbar</div>;
+};
