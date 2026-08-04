32a03ec98686d13cc2626bf0fd73e5a9aed77b9d
vais2004 <vkawale2004@gmail.com>
2026-08-04 16:59:11 +0530

    update feat: created the navbar for the right side and the auth and adding logo to it and icons with the help of lucide react

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { navbarStyles as ns } from "../assets/dummyStyles";
 import logoImg from "../assets/logo.png";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import {
+  Calendar,
+  Grid,
+  Home,
+  List,
+  PlusSquare,
+  UserPlus,
+  Users,
+} from "lucide-react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  const [open, setOpen] = useState(false);
+  const navInnerRef = useRef(null);
+  const indicatorRef = useRef(null);
+  const location = useLocation();
+  const navigation = useNavigate();
+
+  return (
+    <header className={ns.header}>
+      <nav className={ns.navContainer}>
+        <div className={ns.flexContainer}>
+          <div className={ns.logoContainer}>
+            <img src={logoImg} alt="logo" className={ns.logoImage} />
+
+            <Link to="/">
+              <div className={ns.logoLink}> Vaidyra</div>
+              <div className={ns.logoSubtext}>Healthcare Solutions</div>
+            </Link>
+          </div>
+          {/* center navigation */}
+          <div className={ns.centerNavContainer}>
+            <div className={ns.glowEffect}>
+              <div className={ns.centerNavInner}>
+                <div
+                  ref={navInnerRef}
+                  tabIndex={0}
+                  className={ns.centerNavScrollContainer}
+                  style={{ WebkitOverflowScrolling: "touch" }}>
+                  <CenterNavItem
+                    to="/h"
+                    label="Dashboard"
+                    icon={<Home size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/add"
+                    label="Add Doctor"
+                    icon={<UserPlus size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/list"
+                    label="List Doctors"
+                    icon={<Users size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/appointments"
+                    label="Appointments"
+                    icon={<Calendar size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/service-dashboard"
+                    label="Service Dashboard"
+                    icon={<Grid size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/add-service"
+                    label="Add Service"
+                    icon={<PlusSquare size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/list-service"
+                    label="List Services"
+                    icon={<List size={16} />}
+                  />
+                  <CenterNavItem
+                    to="/service-appointments"
+                    label="Service Appointments"
+                    icon={<Calendar size={16} />}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Right side*/}
+          <div className={ns.rightContainer}>
+{/* auth */}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
+
+function CenterNavItem({ to, icon, label }) {
+  return (
+    <NavLink
+      to={to}
+      and
+      className={({ isActive }) =>
+        `nav-item ${isActive ? "active" : ""} ${ns.centerNavItemBase} ${isActive ? ns.centerNavItemActive : ns.centerNavItemInactive}`
+      }>
+      <span>{icon}</span>
+      <span className="font-medium">{label}</span>
+    </NavLink>
+  );
+}
