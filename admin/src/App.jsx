b3ffef4a6ee1e0edef1b507f08d8922632a37113
vais2004 <vkawale2004@gmail.com>
2026-08-05 13:05:13 +0530

    update feat: wrote an function for Require Auth

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,8 +2,31 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Hero } from "./pages/Hero";
+import { useUser } from "@clerk/clerk-react";
+
+function RequireAuth({ children }) {
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) return null;
+  if (!isSignedIn)
+    return (
+      <div className="min-h-screen font-mono flex items-center justify-center bg-linear-to-b from-emerald-50 via-green-50 to-emerald-100 px-4">
+        <div className="text-center">
+          <p className="text-emerald-800 font-semibold text-lg sm:text-2xl mb-4 animate-fade-in"></p>
+          <div className="flex justify-center">
+            <Link
+              to="/"
+              className="px-4 py-2 text-sm rounded-full bg-emerald-600 text-white shadow-sm hover:bg-emerald-700 hover:shadow-md transition-all duration-300 ease-in-out animate-bounce-subtle">
+              HOME
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  return children;
+}
 
 function App() {
   const [count, setCount] = useState(0);
