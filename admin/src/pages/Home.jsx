c95f282a664261a8de6b2306dc866ea88df6b514
vais2004 <vkawale2004@gmail.com>
2026-08-05 22:12:03 +0530

    update feat: created an page as Home

---
+++ admin/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+import { DashboardPage } from '../components/DashboardPage'
+
+export const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <DashboardPage/>
+    </div>
+  )
+}
