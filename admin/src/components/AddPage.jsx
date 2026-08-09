08f01c3269a66b984766187b161f26dc23ea6339
vais2004 <vkawale2004@gmail.com>
2026-08-09 17:00:55 +0530

    update Add page

--- admin/src/components/AddPage.jsx
+++ admin/src/components/AddPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { doctorDetailStyles as s} from "../assets/dummyStyles";
+
 
 const AddPage = () => {
-  return <div>AddPage</div>;
+  return (
+  <div>AddPage</div>
+)
 };
 
 export default AddPage;
