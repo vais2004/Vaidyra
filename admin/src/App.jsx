47b687f8a5526e9412c573e018271d7a65ff7c88
vais2004 <vkawale2004@gmail.com>
2026-08-12 23:24:42 +0530

    update feat: wrote the new Route for the List page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,7 @@ import { Hero } from "./pages/Hero";
 import { useUser } from "@clerk/clerk-react";
 import { Home } from "./pages/Home";
 import Add from "./pages/Add";
+import List from "./pages/List";
 
 function RequireAuth({ children }) {
   const { isLoaded, isSignedIn } = useUser();
@@ -51,6 +52,14 @@ function App() {
         element={
           <RequireAuth>
             <Add />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/list"
+        element={
+          <RequireAuth>
+            <List />
           </RequireAuth>
         }
       />
