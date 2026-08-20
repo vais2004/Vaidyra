b8a670df21405a99dbec502b3404835a3bf00b69
vais2004 <vkawale2004@gmail.com>
2026-08-20 20:30:29 +0530

    update feat: create the page for the Service Dashboard and impoted the Navbar and the ServiceDashboardPage from the components folder

---
+++ admin/src/pages/ServiceDashboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import ServiceDashboardPage from "../components/ServiceDashboardPage";
+
+const ServiceDashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <ServiceDashboardPage />
+    </div>
+  );
+};
+
+export default ServiceDashboard;
