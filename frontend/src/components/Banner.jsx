e41c4fe63e125fe1eb5fa7bd99161896cfce85b8
vais2004 <vkawale2004@gmail.com>
2026-08-25 22:06:19 +0530

    update feat : wrote the code for title and stars and for tagline

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import { bannerStyles as s } from "../assets/dummyStyles";
+import { Star, Stethoscope } from "lucide-react";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className={s.bannerContainer}>
+      <div className={s.mainContainer}>
+        <div className={s.borderOutline}>
+          <div className={s.outerAnimatedBand}></div>
+          <div className={s.innerWhiteBorder}></div>
+        </div>
 
-export default Banner
+        <div className={s.contentContainer}>
+          <div className={s.flexContainer}>
+            <div className={s.leftContent}>
+              <div className={s.headerBadgeContainer}>
+                <div className={s.stethoscopeContainer}>
+                  <div className={s.stethoscopeInner}>
+                    <Stethoscope className={s.stethoscopeIcon} />
+                  </div>
+                </div>
+                <div className={s.titleContainer}>
+                  <h1 className={s.title}>
+                    Vaid
+                    <span className={s.titleGradient}>yra+</span>
+                  </h1>
+                  {/* stars */}
+                  <div className={s.starsContainer}>
+                    <div className={s.starsInner}>
+                      {[1, 2, 3, 4, 5].map((star) => (
+                        <Star className={s.starIcon} key={star} />
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* tagline */}
+              <p className={s.tagline}>
+                Premium Healthcare
+                <span className={`block ${s.taglineHighlight}`}>
+                  At Your Fingertips
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
