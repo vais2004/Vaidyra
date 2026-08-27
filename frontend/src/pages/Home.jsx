99d58589fb996df3582a96c3688d9689ad4cc883
vais2004 <vkawale2004@gmail.com>
2026-08-27 22:33:22 +0530

    update feat: imported the certification from the folder components and add it to the Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
+import Certification from "../components/Certification";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Banner />
+      <Certification />
     </div>
   );
 };
