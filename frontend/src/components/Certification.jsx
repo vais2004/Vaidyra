7d5963bc34ea052eab8c19d7ab72842538744d14
vais2004 <vkawale2004@gmail.com>
2026-08-30 18:41:30 +0530

    update

--- frontend/src/components/Certification.jsx
+++ frontend/src/components/Certification.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { certificationStyles as s } from "../assets/dummyStyles";
+import C3 from "../assets/C3.png";
+import C1 from "../assets/C1.png";
+import C2 from "../assets/C2.png";
+import C4 from "../assets/C4.svg";
+import C5 from "../assets/C5.png";
+import C6 from "../assets/C6.png";
+import C7 from "../assets/C7.svg";
 
 const Certification = () => {
-  return (
-    <div>Certification</div>
-  )
-}
+  const certifications = [
+    { id: 1, name: "Medical Commission", image: C1, type: "international" },
+    { id: 2, name: "Government Approved", image: C2, type: "government" },
+    {
+      id: 3,
+      name: "NABH Accredited",
+      image: C3,
+      alt: "NABH Accreditation",
+      type: "healthcare",
+    },
+    { id: 4, name: "Medical Council", image: C4, type: "government" },
+    {
+      id: 5,
+      name: "Quality Healthcare",
+      image: C5,
+      alt: "Quality Healthcare",
+      type: "healthcare",
+    },
+    {
+      id: 6,
+      name: "Paramedical Council",
+      image: C6,
+      alt: "Patient Safety",
+      type: "healthcare",
+    },
+    {
+      id: 7,
+      name: "Ministry of Health",
+      image: C7,
+      alt: "Ministry of Health",
+      type: "government",
+    },
+  ];
 
-export default Certification
+  const duplicatedCertifications = [
+    ...certifications,
+    ...certifications,
+    ...certifications,
+  ];
+
+  return (
+    <div className={s.container}>
+      <div className={s.backgroundGrid}>
+        <div className={s.topLine}>
+          <div className={s.gridContainer}>
+            <div className={s.grid}>
+              {Array.from({ length: 144 }).map((_, i) => (
+                <div key={i} className={s.gridCell}></div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={s.contentWrapper}>
+          <div className={s.headingContainer}>
+            <div className={s.headingInner}>
+              <div className={s.leftLine}></div>
+              <div className={s.rightLine}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Certification;
