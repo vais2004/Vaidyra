c052d279b61a82f9faa5948ac7f4f1f8afc30aa9
vais2004 <vkawale2004@gmail.com>
2026-08-31 11:48:45 +0530

    update feat: imported the HomeDoctors for the components folder

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Certification from "../components/Certification";
+import HomeDoctors from "../components/HomeDoctors";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Banner />
       <Certification />
+      <HomeDoctors />
     </div>
   );
 };
