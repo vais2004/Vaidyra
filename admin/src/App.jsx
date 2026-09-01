494d9871db4bb5163037f73311d87fbcce0be4e4
vais2004 <vkawale2004@gmail.com>
2026-09-01 12:58:24 +0530

    update feat: removing the unecessary imports from the page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import { Link, Route, Routes } from "react-router-dom";
 import { Hero } from "./pages/Hero";
 import { useUser } from "@clerk/clerk-react";
