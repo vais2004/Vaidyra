91e036195ead1de02d1a994a793dcff5472f5e77
vais2004 <vkawale2004@gmail.com>
2026-09-01 15:03:05 +0530

    update feat: adding the title and the subtitle to the component

--- frontend/src/components/Testimonial.jsx
+++ frontend/src/components/Testimonial.jsx
@@ -147,8 +147,14 @@ const Testimonial = () => {
   );
 
   return (
-    <div>
-      <div></div>
+    <div className={s.container}>
+      <div className={s.headerContainer}>
+        <h2 className={s.title}>Voices of Trust</h2>
+        <p className={s.subtitle}>
+          Real stories from doctors and patients sharing their positive
+          experience with out healthcare platform.
+        </p>
+      </div>
     </div>
   );
 };
