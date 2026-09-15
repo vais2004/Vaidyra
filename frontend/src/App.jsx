8f607f748da101093be49501f158a067b33eb03a
vais2004 <vkawale2004@gmail.com>
2026-09-15 16:55:45 +0530

    Update feat: created the route for the Doctors page as /doctors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
       </Routes>
     </div>
   );
